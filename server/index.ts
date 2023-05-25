88791204526e8633153892bd9da72fd3aaffae68
Aashish-Upadhyay-101 <boysmall300@gmail.com>
2023-05-25 23:02:32 +0545

    procedures setup

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,26 @@
-import { router } from "./trpc";
+import { router, publicProcedure } from "./trpc";
+import { PrismaClient } from "@prisma/client";
+import { z } from "zod";
 
-const appRouter = router({});
+const prisma = new PrismaClient();
+
+const appRouter = router({
+  getAllUser: publicProcedure.query(async () => {
+    const users = await prisma.user.findMany();
+    return users;
+  }),
+
+  getUserById: publicProcedure.input(z.number()).query(async (id) => {
+    const { input } = id;
+    console.log(id);
+    console.log(input);
+    const user = await prisma.user.findFirstOrThrow({
+      where: {
+        id: input,
+      },
+    });
+    return user;
+  }),
+});
 
 export type AppRouter = typeof appRouter;
